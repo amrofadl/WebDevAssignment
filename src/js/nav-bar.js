48d0a5b7b6ea86fc068b9157e870231b7bc9256a
fiyn <alienafis@gmail.com>
2022-04-03 23:46:13 +0800

    working on nav-bar and dashboard

---
+++ src/js/nav-bar.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    // let header = document.getElementById("myDiv");
+    // console.log(header);
+    // let btns = document.querySelector("page-button");
+    // for (let i=0; i < btns.length; i++) {
+    //     btns[i].addEventListener("click", function() {
+    //         let current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
+    // }
+
+    // const current = 0;
+    // for (let i = 0; i < document.links.length; i++) {
+    //     if (document.links[i].href === document.URL) {
+    //         current = i;
+    //     }
+    // }
+    // document.links[current].classList.add("active");
+});
+
