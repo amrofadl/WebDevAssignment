fd962fbab256ce0378650085c1e0df5a861aff42
fiyn <alienafis@gmail.com>
2022-04-04 15:01:25 +0800

    edit some stuffs

--- src/js/student-project.js
+++ src/js/student-project.js
@@ -26,25 +26,25 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-    let inputs = document.querySelectorAll( '.inputfile' );
-    Array.prototype.forEach.call( inputs, function( input )
-    {
-	var label	 = input.nextElementSibling,
-		labelVal = label.innerHTML;
+//     let inputs = document.querySelectorAll( '.inputfile' );
+//     Array.prototype.forEach.call( inputs, function( input )
+//     {
+// 	var label	 = input.nextElementSibling,
+// 		labelVal = label.innerHTML;
 
-	input.addEventListener( 'change', function( e )
-	{
-		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-		else
-			fileName = e.target.value.split( ' \ ').pop();
+// 	input.addEventListener( 'change', function( e )
+// 	{
+// 		var fileName = '';
+// 		if( this.files && this.files.length > 1 )
+// 			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+// 		else
+// 			fileName = e.target.value.split( ' \ ').pop();
 
-		if( fileName )
-			label.querySelector( 'span' ).innerHTML = fileName;
-		else
-			label.innerHTML = labelVal;
-	});
-});
+// 		if( fileName )
+// 			label.querySelector( 'span' ).innerHTML = fileName;
+// 		else
+// 			label.innerHTML = labelVal;
+// 	});
+// });
 
 });
