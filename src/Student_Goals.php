6ed5987dba2fe6abe175f056d86e0220242e5f3d
fiyn <alienafis@gmail.com>
2022-04-10 16:29:22 +0800

    worked on nav-bar active button and moved into inc.php

--- src/Student_Goals.php
+++ src/Student_Goals.php
@@ -8,32 +8,12 @@
     <link rel="stylesheet" href="./css/nav-bar.css">
     <link rel="stylesheet" href="./css/student_goals.css">
     <script src="https://kit.fontawesome.com/3d7a902bb5.js" crossorigin="anonymous"></script>
-
+    <script src="./js/nav-bar.js"></script>
     <title>Goals</title>
 </head>
 <body>
-    <div class="nav-bar">
-        <div class="nav-bar__user-div">
-            <div>
-                <img class="" src="./images/User.png" alt="User Icon">
-            </div>
-            <div id="userType">UserType - Student</div>
-        </div>
-        <div class="wrapper__page-button">
-            <div class="nav-bar__page-list">
-                <ul id="myDiv">
-                    <li><a class="page-button" href="./Student_Dashboard.php"><span class="page-icon"><i class="fa-brands fa-trello"></i></span>Dashboard</a></li>
-                    <li><a class="page-button" href="./Student_Project.php"><span class="page-icon"><i class="fa-solid fa-briefcase"></i></span>Projects</a></li>
-                    <li><a class="page-button" href="./Student_Meeting.php"><span class="page-icon"><i class="fa-solid fa-calendar-days"></i></span>Meetings</a></li>
-                    <li><a class="page-button active" href="./Student_Goals.php"><span class="page-icon"><i class="fa-solid fa-bullseye"></i></span>Goals</a></li>
-                    <li><a class="page-button" href="./Student_Assessment.php"><span class="page-icon"><i class="fa-solid fa-clipboard-list"></i></span>Assessment</a></li>
-                </ul>
-            </div>
-            <div class="wrapper__sign-out">
-                <a id="signout" href="login.php">Sign Out</a>
-            </div>
-        </div>
-    </div>
+    
+    <?php include 'includes/../php/student_nav-bar.inc.php'; ?>
 
     <div class="container__content">
         <div class="container__goals">
--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -1,21 +1,13 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    // let header = document.getElementById("myDiv");
-    // console.log(header);
-    // let btns = document.querySelector("page-button");
-    // for (let i=0; i < btns.length; i++) {
-    //     btns[i].addEventListener("click", function() {
-    //         let current = document.getElementsByClassName("active");
-    //         current[0].className = current[0].className.replace(" active", "");
-    //         this.className += " active";
-    //     });
-    // }
 
-    // const current = 0;
-    // for (let i = 0; i < document.links.length; i++) {
-    //     if (document.links[i].href === document.URL) {
-    //         current = i;
-    //     }
-    // }
-    // document.links[current].classList.add("active");
+    const activePage = window.location.pathname;
+    console.log(activePage);
+    const navLinks = document.querySelectorAll("ul a").forEach(link => {
+        console.log(link.href);
+        if(link.href.includes(`${activePage}`)) {
+            console.log(`${activePage}`);
+            link.classList.add("active");
+        }
+    });
 });
 
