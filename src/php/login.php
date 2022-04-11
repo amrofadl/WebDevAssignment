49434ef7e24b5d667ca0e3671c91ebab6c6f8898
ewrn <91323579+ewrn@users.noreply.github.com>
2022-04-12 00:57:58 +0800

    fix login with wrong password

--- src/php/login.php
+++ src/php/login.php
@@ -9,31 +9,25 @@
 
     if ($email_student->num_rows > 0) {
         $student = $email_student->fetch_assoc();
-
-        if (strcmp($password, $student['password'] == 0)) {
+        if ($password == $student['password']) {
             session_start();
             $_SESSION['email'] = $student['email_address'];
             $_SESSION['username'] = $student['username'];
             $_SESSION['student_id'] = $student['student_id'];
 
             echo "student";
-        }else {
-            echo "Wrong email or password";
         }
     }
     
     if ($email_supervisor->num_rows > 0) {
         $supervisor = $email_supervisor->fetch_assoc();
-        
-        if (strcmp($password, $supervisor['password'] == 0)) {
+        if ($password == $supervisor['password']) {
             session_start();
             $_SESSION['email'] = $supervisor['email_address'];
             $_SESSION['username'] = $supervisor['username'];
             $_SESSION['supervisor_id'] = $supervisor['supervisor_id'];
 
             echo "supervisor";
-        }else {
-            echo "Wrong email or password";
         }
     }
 ?>
