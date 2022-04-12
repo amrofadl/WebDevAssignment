a541e4251ef5dc71ce19830f9837f3518c8e32b1
fiyn <alienafis@gmail.com>
2022-04-12 12:16:42 +0800

    add input validation for assessment mark

--- src/Supervisor_Assessment.php
+++ src/Supervisor_Assessment.php
@@ -25,8 +25,8 @@
             <hr>
             <div class="container__table-view">
                 <div class="container__student-table">
+                    <h2 class="title">Students List</h2>
                     <table class="table student-table">
-                        <caption>Students List</caption>
                         <thead>
                             <tr>
                                 <th>Student</th>
@@ -38,7 +38,7 @@
                         <tbody>
                             <tr>
                                 <td>Sam</td>
-                                <td>98</td>
+                                <td>0</td>
                                 <td>
                                     <button class="table-edit-button" type="button"><span class="icon"><i class="fa-solid fa-eye"></i></span>View</button>
                                 </td>
@@ -57,18 +57,22 @@
                 </div>
                
                 <div class="container__marksheet">
-                    <form class="form form__marksheet" action="" method="post">
+                    <form class="form form__marksheet" action="" method="post" id="markForm">
                         <h3 class="title container__title">Student Marksheet</h3>
                         <!-- <div class="form__message form__message--error">Incorrect username/password combination</div> -->
                         <div class="details">
-                            <h3 class="title">Details</h3>
+                            <h3 class="-title">Details</h3>
                             <hr>
-                            <p>Name: Sam Willy</p>
-                            <p>Project Title: XXXXXS</p>
+                            <p>Name: 
+                                <span>Sam Willy</span>
+                            </p>
+                            <p>Project Title: 
+                                <span>XXXXXS</span>
+                            </p>
                         </div>
 
                         <div class="container__evaluation-table">
-                            <h3 class="title">Evaluation Criteria</h3>
+                            <h3 class="">Evaluation Criteria</h3>
                             <hr>
                             <table class="table evaluation-table">
                                 <thead>
@@ -79,49 +83,49 @@
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>Criteria A</td>
+                                        <td>Definition of Problem</td>
                                         <td>
-                                            <input class="mark" type="text" placeholder="enter mark" value="32" maxlength="2" size="2" disabled>
+                                            <input class="mark" type="number" value="0" min="0" max="25" id="criteriaA" disabled>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td>Criteria B</td>
+                                        <td>Narrative and Flow Charts or Pseudocode</td>
                                         <td>
-                                            <input class="mark" type="text" placeholder="enter mark" value="32" maxlength="2" size="2" disabled>
+                                            <input class="mark" type="number" value="12" min="0" max="25" disabled>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td>Criteria C</td>
+                                        <td>Coding of Program</td>
                                         <td>
-                                            <input class="mark" type="text" placeholder="enter mark" value="32" maxlength="2" size="2" disabled>
+                                            <input class="mark" type="number" value="0" min="0" max="25" disabled>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td>Criteria D</td>
+                                        <td>Testing and Presentation</td>
                                         <td>
-                                            <input class="mark" type="text" placeholder="enter mark" value="31" maxlength="2" size="2" disabled>
+                                            <input class="mark" type="number" value="0" min="0" max="25" disabled>
                                         </td>
                                     </tr>
-                                    <tr>
+                                    <tr class="total">
                                         <td>Total Mark</td>
-                                        <td> 98
+                                        <td> 0
                                             <!-- <input type="text" placeholder="enter mark" value="12" maxlength="2" size="2" disabled> -->
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
+
                         <div class="container__button">
                             <div class="button__left-group">
-                                <button id="editBtn" class="button evaluation-button button-edit"><span class="icon"><i class="fa-solid fa-pen-to-square"></i></span>Edit</button>
-                                <button id="cancelBtn" class="button evaluation-button button-cancel"><span class="icon"><i class="fa-solid fa-ban"></i></span>Cancel</button>
+                                <button id="editBtn" class="button evaluation-button button-edit" form="markForm" value="Edit"><span class="icon"><i class="fa-solid fa-pen-to-square"></i></span>Edit</button>
+                                <button id="cancelBtn" class="button evaluation-button button-cancel" type="reset" form="markForm" value="Reset"><span class="icon"><i class="fa-solid fa-ban"></i></span>Cancel</button>
                             </div>
                             <div class="button__right-group">
-                                <button class="button evaluation-button button-save" type="submit"><span class="icon"><i class="fa-solid fa-floppy-disk"></i></span>Save</button>
+                                <button class="button evaluation-button button-save" type="submit" form="markForm"><span class="icon"><i class="fa-solid fa-floppy-disk"></i></span>Save</button>
                             </div>
                         </div>
                     </form>
-
                 </div>
             </div>
         </div>
