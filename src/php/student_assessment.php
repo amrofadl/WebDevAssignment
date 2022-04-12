e3722fe5757426aecd350948126bca32ebffa280
ewrn <91323579+ewrn@users.noreply.github.com>
2022-04-12 11:05:30 +0800

    small fix

--- src/php/student_assessment.php
+++ src/php/student_assessment.php
@@ -1,7 +1,6 @@
 <?php
     require 'config.php';
 
-    session_start();
     $id = $_SESSION['student_id'];
     $student_assessment = $link->query("SELECT * FROM assessment WHERE student_id = '$id'");
 
